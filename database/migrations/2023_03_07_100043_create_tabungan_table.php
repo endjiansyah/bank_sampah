ff2820e19590ad438195dd0f6e4c61bf96c959dc
endjiansyah <endji1507@gmail.com>
2023-03-07 17:06:58 +0700

    buat migration & model

---
+++ database/migrations/2023_03_07_100043_create_tabungan_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tabungan', function (Blueprint $table) {
+            $table->id();
+            $table->integer("id_nasabah");
+            $table->integer("sampah");
+            $table->integer("debet")->nullable();
+            $table->integer("kredit")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tabungan');
+    }
+};
